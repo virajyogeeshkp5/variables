2994c8135ac82995a3686800057df6df0df8ec37
YOGEESH K P <yogeesh793@gmail.com>
2024-11-25 18:49:12 +0530

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,85 @@
+# Integer
+a=2
+print(a)
+
+b=123456748
+print(b)
+
+# Float
+pi=3.24
+print(pi)
+
+# Boolean
+n=True
+print(n)
+
+# String
+s='A'
+print(s)
+
+s='yogee'
+print(s)
+
+# Enmpty value or Null data type
+x=None
+print(x)
+
+import keyword
+print(keyword.kwlist)
+
+# Integer
+a=2
+print(a)
+print(type(a))
+
+b=123456748
+print(b)
+print(type(b))
+
+# Float
+pi=3.24
+print(pi)
+print(type(pi))
+
+# Boolean
+n=True
+print(n)
+print(type(n))
+
+# String
+s='A'
+print(s)
+print(type(s))
+
+s='yogee'
+print(s)
+print(type(s))
+
+# Enmpty value or Null data type
+x=None
+print(x)
+print(type(x))
+
+a,b,c=1,2,3
+print(a,b,c)
+
+a,b,_=1,2,3
+print(a,b)
+
+a=b=c=1
+print(a,b,c)
+
+b=2
+print(a,b,c)
+
+x=y=[7,8,9]
+x=[13,8,9]
+print(y)
+
+x=y=[7,8,9]
+x[0]=13
+print(y)
+
+x=[1,2,[3,4,5],6,7]
+print(x[2])
+print(x[2][1])
